e8c97689b7e31983b5f74585a46f090995a39443
Vlad Kats <mars77@gmail.com>
2026-01-14 20:00:10 -0700

    attempting deploy again #skip

--- index.ts
+++ index.ts
@@ -3,12 +3,12 @@ import logger from './utils/logger'
 import { dbConnect } from './dbConnection'
 import { initDB } from './initDB'
 
-const PORT = process.env.PORT || 3001
+const PORT = parseInt(process.env.PORT || '3001', 10)
 
 ;(async () => {
   await dbConnect()
   if (process.env.NODE_ENV == 'development') await initDB()
-  app.listen(PORT, () => {
+  app.listen(PORT, '0.0.0.0', () => {
     logger.info(`Server running on port ${PORT}`)
   })
 })()
